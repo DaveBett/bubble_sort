438260ca467977dafc18dab461ffe518b1424b9b
DaveBett <davide.betterini@gmail.com>
2025-03-24 11:24:07 +0000

    polished code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,8 @@
-require "pry-byebug"
-def bubble_sort(array)
+# frozen_string_literal: true
 
+require 'pry-byebug'
+def bubble_sort(array)
   bubbled = array
-  temp = 0
   i = 0
 
   while bubbled != array.sort
@@ -12,11 +12,9 @@ def bubble_sort(array)
       bubbled[i + 1] = temp
     end
     i += 1
-    if i + 2 > array.length
-      i = 0
-    end
+    i = 0 if i + 2 > array.length
   end
-return bubbled
+  bubbled
 end
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
